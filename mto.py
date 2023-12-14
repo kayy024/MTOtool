3d97f4e25353b419a94d228d99f811399abc25ac
khadijah4 <kraja40k@gmail.com>
2023-12-14 13:28:13 +0000

    Using tkinter and ttk.Notebook to set gui and make pages, and establishing a database connection

--- mto.py
+++ mto.py
@@ -10,3 +10,22 @@ import spacy
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from itertools import cycle
+
+class MTO:
+    def __init__(self, master):
+        self.master = master
+        master.title("MTO Tool")
+
+        # Creating a notebook for multiple pages
+        self.notebook = ttk.Notebook(master)
+        self.notebook.pack(expand=1, fill="both")
+
+        # This is the Login/Register page
+        self.create_login_page()
+
+        # Here I am initialising the database
+        self.conn = sqlite3.connect("user_data.db")
+        self.create_table()
+
+        # This is a temporary directory for storing CV files
+        self.temp_dir = tempfile.mkdtemp()
